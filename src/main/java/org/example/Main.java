da0fd049f4ac6c399d31387991945a123db81bdd
Guillem <guillem.ms.pego@gmail.com>
2026-01-19 11:26:24 +0100

    Implementat el menu principal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,36 @@
+package org.example;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Git");
+        int opcion;
+        do {
+            opcion = menuPrincipal();
+        } while (opcion != 0);
+    }
+
+    private static int menuPrincipal() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion = -1;
+        do {
+            System.out.println("===MENU TAMAGOTCHI===");
+            System.out.println("1. Estado");
+            System.out.println("2. Comer");
+            System.out.println("3. Jugar");
+            System.out.println("4. Dormir");
+            System.out.println("0. Salir");
+            System.out.println("Seleccione una opción:");
+
+            if (scanner.hasNextInt()) {
+                opcion = scanner.nextInt();
+                if (opcion < 0 || opcion > 4) {
+                    System.out.println("Opción no válida.");
+                }
+            } else {
+                System.out.println("Ha d'introduir un número.");
+                scanner.next();
+            }
+        } while (opcion < 0 || opcion > 4);
+        return opcion;
     }
 }
