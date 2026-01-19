42d141b7d2a9190b8a2739c8bd7e8bdf4e12e550
Guillem <guillem.ms.pego@gmail.com>
2026-01-19 12:04:39 +0100

    Afegida funcionalitat extra: Curar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,9 +11,7 @@ public class Main {
         do {
             opcion = menuPrincipal();
 
-            if (opcion == 1) {
-                mostrarEstado();
-            }
+            if (opcion == 1) mostrarEstado();
 
             if (opcion == 2) {
                 if (saciedad < 10) {
@@ -28,7 +26,7 @@ public class Main {
 
             if (opcion == 3) {
                 if (diversion < 10) {
-                    diversion += 3;
+                    diversion += 3; // Jugar puja diversió
                     if (diversion > 10) diversion = 10;
                     saciedad -= 1;
                     energia -= 2;
@@ -50,6 +48,14 @@ public class Main {
                 }
             }
 
+            if (opcion == 5) {
+                System.out.println("Visitant al metge... T'has curat!");
+                energia = 10;
+                saciedad = 10;
+                diversion -= 3;
+                mostrarEstado();
+            }
+
         } while (opcion != 0);
     }
 
@@ -62,26 +68,27 @@ public class Main {
             System.out.println("2. Comer");
             System.out.println("3. Jugar");
             System.out.println("4. Dormir");
+            System.out.println("5. Curar (Metge)");
             System.out.println("0. Salir");
             System.out.println("Seleccione una opción:");
 
             if (scanner.hasNextInt()) {
                 opcion = scanner.nextInt();
-                if (opcion < 0 || opcion > 4) {
+                if (opcion < 0 || opcion > 5) {
                     System.out.println("Opción no válida.");
                 }
             } else {
                 System.out.println("Ha d'introduir un número.");
                 scanner.next();
             }
-        } while (opcion < 0 || opcion > 4);
+        } while (opcion < 0 || opcion > 5);
         return opcion;
     }
 
     private static void mostrarEstado() {
         System.out.println("Saciedad: " + saciedad + " | Energia: " + energia + " | Diversion: " + diversion);
 
-        if (saciedad <= 0 || energia <= 0 || diversion <= 0) { // Canviat a <=0 per seguretat
+        if (saciedad <= 0 || energia <= 0 || diversion <= 0) {
             System.out.println("(-_-) Game Over");
             System.out.println("Fin del juego!");
             System.exit(0);
